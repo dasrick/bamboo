8a3460d8abd759ab09a0fdf75b7dc522c66169fb
Enrico Hoffmann <dasrick@gmail.com>
2015-07-01 22:18:41 +0200

    add php5.6

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -36,6 +36,9 @@ tar xvjf ${PHANTOM_JS}.tar.bz2
 ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
 ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
 ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
-# maven 3
-#apt-get install -y maven
 
+# PHP 5.6
+apt-get install python-software-properties
+add-apt-repository ppa:ondrej/php5-5.6
+apt-get -y update
+apt-get -y install php5 php5-mhash php5-mcrypt php5-curl php5-cli php5-mysql php5-gd php5-intl
