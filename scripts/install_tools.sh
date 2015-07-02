02562adaa6096f07466d5235fcb997800dfda871
Enrico Hoffmann <dasrick@gmail.com>
2015-07-02 06:56:28 +0200

    add some options into php56 installation

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -38,7 +38,7 @@ ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
 ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
 
 # PHP 5.6
-apt-get install python-software-properties
-add-apt-repository ppa:ondrej/php5-5.6
+apt-get -y install python-software-properties
+add-apt-repository -y ppa:ondrej/php5-5.6
 apt-get -y update
 apt-get -y install php5 php5-mhash php5-mcrypt php5-curl php5-cli php5-mysql php5-gd php5-intl
